24d55e91869155b3a7248cab634cc89548d0b4e6
felipeleao <felipe@fleao.com.br>
2014-05-29 01:03:16 -0300

    creating initial project structure.
    This version is already running, but must be run through eclipse, since
    it has the list of terms to be searched hard coded.

---
+++ src/main/java/br/uniriotec/ppgi/mapping/searcher/ExtractSynsetID.java
@@ -0,0 +1,68 @@
+package br.uniriotec.ppgi.mapping.searcher;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+
+import edu.mit.jwi.Dictionary;
+import edu.mit.jwi.IDictionary;
+import edu.mit.jwi.item.IIndexWord;
+import edu.mit.jwi.item.IWord;
+import edu.mit.jwi.item.IWordID;
+import edu.mit.jwi.item.POS;
+
+public class ExtractSynsetID {
+	private static IDictionary dict = null;
+	/**
+	 * @param args
+	 * @throws IOException 
+	 */
+	public static void main(String[] args) throws IOException {
+		
+		List<String> listaModifiers = new ArrayList<String>();
+		listaModifiers.add("Extraterrestrial Object");
+		listaModifiers.add("Celestial Body");
+		
+		
+		//Instanciar Dicionario
+		IDictionary dict = dicitionaryFactory();
+		
+		// get the synset
+		for(String termo : listaModifiers){
+			System.out.println("== Termo buscado: "+termo);
+			IIndexWord idxWord = dict.getIndexWord(termo, POS.NOUN); 
+			for(IWordID wordID : idxWord.getWordIDs()){
+				IWord word = dict.getWord(wordID);
+				System.out.println(word.getSynset());
+			}
+			System.out.println("----");
+		}
+		
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	public static IDictionary dicitionaryFactory() throws IOException{
+		if(dict == null){
+			System.out.println("Instanciando Dicionario...");
+			// construct the URL to the Wordnet dictionary directory
+			String wnhome = System.getenv("WNHOME");
+			String path = wnhome + File.separator + "dict"; 
+			URL url = new URL("file", null, path);
+			// construct the dictionary object and open it
+			dict = new Dictionary(url); 
+			dict.open();
+		}
+		return dict;
+	}
+	
+
+
+}
